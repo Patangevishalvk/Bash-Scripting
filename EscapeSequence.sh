66bfd863595f3ea3d5dc6df5545ee2743c5c8829
Vishal Patange <vishalpatange69@gmail.com>
2023-08-08 10:41:40 +0530

    Escape Sequence

---
+++ EscapeSequence.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+echo Line 1
+echo Line 2
+echo Line 3
+
+echo -e "Line4\nLine6" 
+
--- Mycode.sh
+++ Mycode.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
+
+# This is Sample Code for Linux
 echo This is my First Code on Linux Machine  
 
 echo Line 1 Vishal
